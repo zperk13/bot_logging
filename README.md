17958deb9791b7849c34ef2760d3e72b2c976d78
zperk13 <zperk13>
2023-06-10 21:36:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Logging code shared between my bots. If you're not me, you probably don't want to use this directly, but you're free to use it as a reference.
---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+#![warn(clippy::cast_possible_truncation)]
+#![warn(clippy::exit)]
+#![cfg_attr(not(test), warn(clippy::expect_used))]
+#![warn(clippy::fallible_impl_from)]
+#![cfg_attr(not(test), warn(clippy::index_refutable_slice))]
+#![cfg_attr(not(test), warn(clippy::indexing_slicing))]
+#![cfg_attr(not(test), warn(clippy::integer_arithmetic))]
+#![cfg_attr(not(test), warn(clippy::missing_panics_doc))]
+#![cfg_attr(not(test), warn(clippy::panic))]
+#![warn(clippy::unchecked_duration_subtraction)]
+#![cfg_attr(not(test), warn(clippy::unreachable))]
+#![cfg_attr(not(test), warn(clippy::unwrap_used))]
+
+use tracing_subscriber::prelude::*;
+
+pub use opentelemetry::global::shutdown_tracer_provider;
+
+pub struct DropHandler;
+
+impl Drop for DropHandler {
+    fn drop(&mut self) {
+        shutdown_tracer_provider();
+    }
+}
+
+pub fn init(project_name: &str) -> Result<DropHandler, opentelemetry::trace::TraceError> {
+    let tracer = opentelemetry_jaeger::new_collector_pipeline()
+        .with_endpoint("http://192.168.1.100:14268/api/traces")
+        .with_reqwest()
+        .with_service_name(project_name)
+        .install_batch(opentelemetry::runtime::Tokio)?;
+
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    let rolling_file_appender = tracing_appender::rolling::RollingFileAppender::new(
+        tracing_appender::rolling::Rotation::DAILY,
+        "./logs",
+        format!("log_{project_name}_"),
+    );
+
+    let owned_project_name = project_name.to_owned();
+    tracing_subscriber::Registry::default()
+        .with(tracing_subscriber::fmt::layer().compact().with_filter(
+            tracing_subscriber::filter::filter_fn(move |metadata| match metadata.module_path() {
+                Some(module_path) => {
+                    module_path.starts_with(&owned_project_name)
+                        && *metadata.level() >= tracing::Level::INFO
+                }
+                None => false,
+            }),
+        ))
+        .with(tracing_subscriber::fmt::layer().with_writer(rolling_file_appender))
+        .with(
+            telemetry.with_filter(tracing_subscriber::filter::filter_fn(
+                |metadata| match metadata.module_path() {
+                    Some(module_path) => module_path.starts_with("ZachBot"),
+                    None => false,
+                },
+            )),
+        )
+        .init();
+    Ok(DropHandler {})
+}
